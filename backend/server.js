7eb597d7e4683cd02b550f8db755cdd73f11fef0
Abdullah Ahmed Khan <abdullahaahmed0539@gmail.com>
2021-02-25 16:26:06 +0500

    MongoDb connection established

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = 'mongodb://localhost:27017/exercise'
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+//step 3: promise        
+const db = mongoose.connection;
+db.once('open', ()=> {
+    console.log("MongoDB database connection established successfully")
+});
 
 app.listen(port, ()=>{
     console.log(`Server running at port ${port}`);
