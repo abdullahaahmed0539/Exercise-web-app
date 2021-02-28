015bab3c2c038ce9e6451dbe6464d7c837dd7fa2
Abdullah Ahmed Khan <abdullahaahmed0539@gmail.com>
2021-02-28 20:40:30 +0500

    frontend created and connected

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/navbar';
+import EditExercise from './components/edit-exercises.component';
+import CreateExercise from './components/createExercise.component';
+import CreateUser from './components/user.component';
+import exList from './components/exercisesList.component';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Navbar/>
+       <br/>
+       <Route path="/" exact component={exList}/>
+       
+       <Route path="/edit/:id" component={EditExercise}/>
+       
+       <Route path="/create" component={CreateExercise}/>
+       
+       <Route path="/user" component={CreateUser}/>
+       
+    </Router>
+   
   );
 }
 
